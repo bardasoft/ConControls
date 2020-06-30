846da7f9e71f9eccbe3dce032ad829512010b4e2
René Vogt <rvogt@aki-gmbh.de>
2020-06-30 10:03:45 +0200

    CodeAnalyzer project setup

---
+++ Sources/ConControlsAnalyzerTests/PublicEntryPoint/PublicEntryPointTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Verify = Microsoft.CodeAnalysis.CSharp.CodeFix.Testing.MSTest.CodeFixVerifier<
+    ConControls.CodeAnalysis.PublicEntryPoint.PublicEntryPointAnalyzer,
+    ConControls.CodeAnalysis.PublicEntryPoint.PublicEntryPointFixProvider>;
+
+namespace ConControlsAnalyzerTests.PublicEntryPoint
+{
+    [TestClass]
+    public class UnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await Verify.VerifyCSharpDiagnosticAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = Verify.Diagnostic("ConControlsAnalyzer").WithLocation(11, 15).WithArguments("TypeName");
+            await Verify.VerifyCSharpFixAsync(test, expected, fixtest);
+        }
+    }
+}
